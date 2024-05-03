d273cb23c8f0f316016f1dd000a9f86ee3ccde3f
Jongita <jurgitajonaityte@gmail.com>
2024-05-03 13:37:39 +0300

    accordeon

--- netflix.js
+++ netflix.js
@@ -1,21 +1,37 @@
-const plusDOM = document.getElementById("plus")
-const answerDOM = document.getElementById("questionAnswer")
-const questionDOM = document.getElementById("questionHeader")
+// const plusDOM = document.getElementById("plus")
+// const answerDOM = document.getElementById("questionAnswer")
+// const questionDOM = document.getElementById("questionHeader")
 
 
-answerDOM.style.display = "none";
+// answerDOM.style.display = "none";
 
-questionDOM.onclick = () => {
-    if (answerDOM.style.display == "none") {
-        answerDOM.style.display = "block"
-        plusDOM.classList.add("x");
-        plusDOM.classList.remove("plus");
-    } else {
-        answerDOM.style.display = "none";
-        plusDOM.classList.add("plus");
-        plusDOM.classList.remove("x");
-    }
+// questionDOM.onclick = () => {
+//     if (answerDOM.style.display == "none") {
+//         answerDOM.style.display = "block"
+//         plusDOM.classList.add("x");
+//         plusDOM.classList.remove("plus");
+//     } else {
+//         answerDOM.style.display = "none";
+//         plusDOM.classList.add("plus");
+//         plusDOM.classList.remove("x");
+//     }
 
+// }
+
+
+const acc = document.querySelectorAll(".acordeon");
+
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active")
+
+        const desc = this.nextElementSibling;
+
+        if (desc.style.maxHeight) {
+            desc.style.maxHeight = null;
+        } else {
+            desc.style.maxHeight = desc.scrollHeight + "px"
+        }
+    })
 }
-
-
